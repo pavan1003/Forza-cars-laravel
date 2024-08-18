ebe7e47c372242a31b3994141911fda1144af7f3
Pavan Mistry <pavan32100@gmail.com>
2024-08-18 16:49:36 -0400

    Created Layout changes

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Forza Horizon 5 Cars</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="/logo.png" type="image/gif">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
+        integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous"
+        async></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" rel="stylesheet">
+    <link rel="stylesheet" href="/styles.css">
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+    @include('layouts.navigation')
+    <div class="container-fluid custom-bg">
+        @yield('content')
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.js"
+        integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script>
+        @if(Session::has('success'))
+                toastr.success("{{ Session::get('success') }}")
+            @endif
+            @if( Session::has('info'))
+                toastr.info("{{ Session::get('info') }}")
+            @endif
+            @if( Session::has('error'))
+                toastr.info("{{ Session::get('error') }}")
+            @endif
+    </script>
+</body>
+
+</html>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,26 @@
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-info-subtle">
+    <div class="container-fluid mx-5">
+        <!-- Brand Logo taken from https://www.flaticon.com/free-icon/3d-car_10490228?term=car&page=3&position=67&origin=tag&related_id=10490228 and Name -->
+        <a class="navbar-brand" href="{{ route('cars.index') }}">
+            <img src="/logo.png" class="pe-2" alt="logo" width="50px">Forza Horizon Cars
+        </a>
+        <!-- Toggler button for collapsing navbar content on small screens -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <!-- Home link -->
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{ route('cars.index') }}">Cars</a>
+                </li>
+                <!-- Drivers link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('drivers.index') }}">Drivers</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
