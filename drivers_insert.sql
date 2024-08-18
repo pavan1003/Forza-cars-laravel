186ccd1b0cf8b7c6c38ed6a10921444aeb41937f
Pavan Mistry <pavan32100@gmail.com>
2024-08-18 14:12:21 -0400

    Temp insert query

---
+++ drivers_insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO `drivers` (`driver_id`, `first_name`, `last_name`, `age`, `country`, `experience_years`, `team`, `car_id`) VALUES
+(1, 'John', 'Doe', 28, 'USA', 5, 'Team Alpha', 1),
+(2, 'Daniel', 'Carvalho', 26, 'Portugal', 5, 'Team Beta', 3),
+(3, 'Max', 'Verstappen', 28, 'Netherlands', 6, 'RB', 5),
+(4, 'Jenson', 'Button', 44, 'England', 15, 'Team Alpha', 10),
+(5, 'Aaron', 'Ramsey', 34, 'Wales', 7, 'Team Beta', 2),
+(6, 'Lewis', 'Hamilton', 39, 'England', 10, 'Team Omega', 12),
+(7, 'Alex', 'Johnson', 24, 'Canada', 3, 'Team Gamma', 3),
+(8, 'Alejandro', 'Gonzalez', 29, 'Spain', 6, 'Team Delta', 4),
+(9, 'Tom', 'Brown', 35, 'Australia', 10, 'Team Epsilon', 5);
