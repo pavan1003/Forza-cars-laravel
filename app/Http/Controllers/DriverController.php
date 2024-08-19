03edeec1c914b1155aa46c9de7289b50fa6597c7
Pavan Mistry <pavan32100@gmail.com>
2024-08-19 00:10:35 -0400

    Fixed Auth

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -14,7 +14,7 @@ class DriverController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->only(['create', 'store', 'edit', 'update', 'destroy']);
     }
 
     /**
