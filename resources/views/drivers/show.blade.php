fd64e178b7079a3b9711df48aeae91e71e2ab3e1
Pavan Mistry <pavan32100@gmail.com>
2024-08-19 00:11:20 -0400

    Added Spacing

--- resources/views/drivers/show.blade.php
+++ resources/views/drivers/show.blade.php
@@ -8,7 +8,7 @@
         </div>
     </div>
 </div>
-<div class="container">
+<div class="container pb-5">
     <div class="card mw-100 glassmorph p-0 rounded-5">
         <div class="row g-0">
             <div class="col-md-12 col-sm-12">
